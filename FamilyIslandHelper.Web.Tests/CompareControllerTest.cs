14b1e4d2d37d146147223786571709fe4780ff74
Pavel Zhuraulevich <p.zhuravlevich@gmail.com>
2024-12-30 23:04:14 +0100

    tests: Add descriptions to Selenium tests

--- FamilyIslandHelper.Web.Tests/CompareControllerTest.cs
+++ FamilyIslandHelper.Web.Tests/CompareControllerTest.cs
@@ -16,6 +16,39 @@ namespace FamilyIslandHelper.Web.Tests
 		}
 
 		[Test]
+		[Description(@"
+			1. Navigate to page Compare of FamilyIslandHelper.Web.
+			2. Select API version.
+			3. Check Component 1.
+				3.1. Select building.
+					3.1.1. Check that building is selected.
+					3.1.2. Check that the 1st item from this building is selected.
+					3.1.3. Check item info.
+					3.1.4. Check that another item from this building is not selected.
+				3.2. Select Item 2 from current building.
+					3.2.1. Check that Item 2 from this building is selected.
+					3.2.2. Check item info.
+				3.3. Turn on showing of list of components.
+					3.3.1. Check that item info contains Components info.
+				3.4. Set ItemCount = 2.
+					3.4.1. Check that item info is updated.
+			4. Turn off showing of list of components.
+			5. Check Component 2.
+				5.1. Select building.
+					5.1.1. Check that building is selected.
+					5.1.2. Check that the 1st item from this building is selected.
+					5.1.3. Check item info.
+					5.1.4. Check that another item from this building is not selected.
+				5.2. Select Item 2 from current building.
+					5.2.1. Check that Item 2 from this building is selected.
+					5.2.2. Check item info.
+				5.3. Turn on showing of list of components.
+					5.3.1. Check that item info contains Components info.
+				5.4. Set ItemCount = 2.
+					5.4.1. Check that item info is updated.
+			6. Check item compare info.
+			7. Make screenshot.
+		")]
 		[TestCase("ApiVersionCompare1", "JewelryWorkshop", "PearlEarrings", "01:15:00, 180", "SapphireBracelet", "01:15:00, 485", "02:30:00, 970", 1)]
 		[TestCase("ApiVersionCompare2", "Forge", "Needle", "00:08:00, 216", "Glass", "00:35:00, 1928", "01:10:00, 3856", 2)]
 		public void ComparePageTest(string apiVersionId, string buildingId, string item1Id, string item1Info, string item2Id, string item2Info, string item1InfoFor2, int testCaseNumber)
@@ -44,21 +77,20 @@ namespace FamilyIslandHelper.Web.Tests
 			building = component.FindElement(By.Id(buildingId));
 			Assert.That(building.GetAttribute("class"), Is.EqualTo(ClassesForSelectedBuilding));
 
-			var item = component.FindElement(By.Id(item1Id));
-			Assert.That(item.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
+			var item1 = component.FindElement(By.Id(item1Id));
+			Assert.That(item1.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 			var itemInfo = component.FindElement(By.Id(itemInfoId));
 			Assert.That(itemInfo.Text, Does.Contain(item1Info));
 
-			item = component.FindElement(By.Id(item2Id));
-			Assert.That(item.GetAttribute("class"), Is.EqualTo("itemNameImage"));
-			item.Click();
+			var item2 = component.FindElement(By.Id(item2Id));
+			Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage"));
+			item2.Click();
 			component = driver.FindElement(By.Id(componentId));
-			item = component.FindElement(By.Id(item2Id));
-			Assert.That(item.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
+			item2 = component.FindElement(By.Id(item2Id));
+			Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 			itemInfo = component.FindElement(By.Id(itemInfoId));
-
 			Assert.That(itemInfo.Text, Does.Contain(item2Info));
 
 			driver.FindElement(By.Id("ShowListOfComponentsForAll")).Click();
--- FamilyIslandHelper.Web.Tests/HomeControllerTest.cs
+++ FamilyIslandHelper.Web.Tests/HomeControllerTest.cs
@@ -17,6 +17,25 @@ namespace FamilyIslandHelper.Web.Tests
 		}
 
 		[Test]
+		[Description(@"
+			1. Navigate to FamilyIslandHelper.Web.
+				1.1. Check buildings count.
+			2. Select API version.
+				2.1. Check buildings count.
+			3. Select Building.
+				3.1. Check that building is selected.
+				3.2. Check that the 1st item from this building is selected.
+				3.3. Check item info.
+				3.4. Check that another item from this building is not selected.
+			4. Select Item 2 from current building.
+				4.1. Check that Item 2 from this building is selected.
+				4.2. Check item info.
+			5. Turn on showing of list of components.
+				5.1. Check that item info contains Components info.
+			6. Set ItemCount = 2.
+				6.1. Check that item info is updated.
+			7. Make screenshot.
+		")]
 		[TestCase("ApiVersionHome1", 13, "JewelryWorkshop", "PearlEarrings", "01:15:00, 180", "SapphireBracelet", "01:15:00, 485", "02:30:00, 970", 1)]
 		[TestCase("ApiVersionHome2", 17, "Forge", "Needle", "00:08:00, 216", "Glass", "00:35:00, 1928", "01:10:00, 3856", 2)]
 		public void HomePageTest(string apiVersionId, int buildingsCount, string buildingId, string item1Id, string item1Info, string item2Id, string item2Info, string item1InfoFor2, int testCaseNumber)
@@ -33,20 +52,19 @@ namespace FamilyIslandHelper.Web.Tests
 			building = driver.FindElement(By.Id(buildingId));
 			Assert.That(building.GetAttribute("class"), Is.EqualTo(ClassesForSelectedBuilding));
 
-			var item = driver.FindElement(By.Id(item1Id));
-			Assert.That(item.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
+			var item1 = driver.FindElement(By.Id(item1Id));
+			Assert.That(item1.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 			var itemInfo = driver.FindElement(By.Id(ItemInfoId));		
 			Assert.That(itemInfo.Text, Does.Contain(item1Info));
 
-			item = driver.FindElement(By.Id(item2Id));
-			Assert.That(item.GetAttribute("class"), Is.EqualTo("itemNameImage"));
-			item.Click();
-			item = driver.FindElement(By.Id(item2Id));
-			Assert.That(item.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
+			var item2 = driver.FindElement(By.Id(item2Id));
+			Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage"));
+			item2.Click();
+			item2 = driver.FindElement(By.Id(item2Id));
+			Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 			itemInfo = driver.FindElement(By.Id(ItemInfoId));
-
 			Assert.That(itemInfo.Text, Does.Contain(item2Info));
 
 			driver.FindElement(By.Id("ShowListOfComponents")).Click();
