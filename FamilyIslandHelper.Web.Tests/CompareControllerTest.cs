4b4c86182a8315146cf0e89dfdc54a32412384cd
Pavel Zhuraulevich <p.zhuravlevich@gmail.com>
2024-12-31 02:32:33 +0100

    tests: Add ability to make screenshot when test is failed

--- FamilyIslandHelper.Web.Tests/CompareControllerTest.cs
+++ FamilyIslandHelper.Web.Tests/CompareControllerTest.cs
@@ -47,23 +47,29 @@ namespace FamilyIslandHelper.Web.Tests
 				5.4. Set ItemCount = 2.
 					5.4.1. Check that item info is updated.
 			6. Check item compare info.
-			7. Make screenshot.
 		")]
 		[TestCase("ApiVersionCompare1", "JewelryWorkshop", "PearlEarrings", "01:15:00, 180", "SapphireBracelet", "01:15:00, 485", "02:30:00, 970", 1)]
 		[TestCase("ApiVersionCompare2", "Forge", "Needle", "00:08:00, 216", "Glass", "00:35:00, 1928", "01:10:00, 3856", 2)]
 		public void ComparePageTest(string apiVersionId, string buildingId, string item1Id, string item1Info, string item2Id, string item2Info, string item1InfoFor2, int testCaseNumber)
 		{
-			driver.FindElement(By.Id(apiVersionId)).Click();
+			try
+			{
+				driver.FindElement(By.Id(apiVersionId)).Click();
 
-			CheckComponent(buildingId, item1Id, item1Info, item2Id, item2Info, item1InfoFor2, "Component1", "ItemInfo1", "ItemCount1");
+				CheckComponent(buildingId, item1Id, item1Info, item2Id, item2Info, item1InfoFor2, "Component1", "ItemInfo1", "ItemCount1");
 
-			driver.FindElement(By.Id("ShowListOfComponentsForAll")).Click();
+				driver.FindElement(By.Id("ShowListOfComponentsForAll")).Click();
 
-			CheckComponent(buildingId, item1Id, item1Info, item2Id, item2Info, item1InfoFor2, "Component2", "ItemInfo2", "ItemCount2");
+				CheckComponent(buildingId, item1Id, item1Info, item2Id, item2Info, item1InfoFor2, "Component2", "ItemInfo2", "ItemCount2");
 
-			Assert.That(driver.FindElement(By.Id("ItemCompareInfo")).Text, Is.Not.Empty);
+				Assert.That(driver.FindElement(By.Id("ItemCompareInfo")).Text, Is.Not.Empty);
+			}
+			catch
+			{
+				MakeScreen($"TestCase{testCaseNumber}_Exception");
 
-			MakeScreen($"TestCase{testCaseNumber}Finish");
+				throw;
+			}
 		}
 
 		private void CheckComponent(string buildingId, string item1Id, string item1Info, string item2Id, string item2Info, string item1InfoFor2, string componentId, string itemInfoId, string itemCountId)
--- FamilyIslandHelper.Web.Tests/HomeControllerTest.cs
+++ FamilyIslandHelper.Web.Tests/HomeControllerTest.cs
@@ -34,50 +34,56 @@ namespace FamilyIslandHelper.Web.Tests
 				5.1. Check that item info contains Components info.
 			6. Set ItemCount = 2.
 				6.1. Check that item info is updated.
-			7. Make screenshot.
 		")]
 		[TestCase("ApiVersionHome1", 13, "JewelryWorkshop", "PearlEarrings", "01:15:00, 180", "SapphireBracelet", "01:15:00, 485", "02:30:00, 970", 1)]
 		[TestCase("ApiVersionHome2", 17, "Forge", "Needle", "00:08:00, 216", "Glass", "00:35:00, 1928", "01:10:00, 3856", 2)]
 		public void HomePageTest(string apiVersionId, int buildingsCount, string buildingId, string item1Id, string item1Info, string item2Id, string item2Info, string item1InfoFor2, int testCaseNumber)
 		{
-			Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(17));
+			try
+			{
+				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(17));
 
-			driver.FindElement(By.Id(apiVersionId)).Click();
+				driver.FindElement(By.Id(apiVersionId)).Click();
 
-			Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(buildingsCount));
+				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(buildingsCount));
 
-			var building = driver.FindElement(By.Id(buildingId));
-			building.Click();
+				var building = driver.FindElement(By.Id(buildingId));
+				building.Click();
 
-			building = driver.FindElement(By.Id(buildingId));
-			Assert.That(building.GetAttribute("class"), Is.EqualTo(ClassesForSelectedBuilding));
+				building = driver.FindElement(By.Id(buildingId));
+				Assert.That(building.GetAttribute("class"), Is.EqualTo(ClassesForSelectedBuilding));
 
-			var item1 = driver.FindElement(By.Id(item1Id));
-			Assert.That(item1.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
+				var item1 = driver.FindElement(By.Id(item1Id));
+				Assert.That(item1.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
-			var itemInfo = driver.FindElement(By.Id(ItemInfoId));		
-			Assert.That(itemInfo.Text, Does.Contain(item1Info));
+				var itemInfo = driver.FindElement(By.Id(ItemInfoId));
+				Assert.That(itemInfo.Text, Does.Contain(item1Info));
 
-			var item2 = driver.FindElement(By.Id(item2Id));
-			Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage"));
-			item2.Click();
-			item2 = driver.FindElement(By.Id(item2Id));
-			Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
+				var item2 = driver.FindElement(By.Id(item2Id));
+				Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage"));
+				item2.Click();
+				item2 = driver.FindElement(By.Id(item2Id));
+				Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
-			itemInfo = driver.FindElement(By.Id(ItemInfoId));
-			Assert.That(itemInfo.Text, Does.Contain(item2Info));
+				itemInfo = driver.FindElement(By.Id(ItemInfoId));
+				Assert.That(itemInfo.Text, Does.Contain(item2Info));
 
-			driver.FindElement(By.Id("ShowListOfComponents")).Click();
-			itemInfo = driver.FindElement(By.Id(ItemInfoId));
-			Assert.That(itemInfo.Text, Does.Contain("Components"));
+				driver.FindElement(By.Id("ShowListOfComponents")).Click();
+				itemInfo = driver.FindElement(By.Id(ItemInfoId));
+				Assert.That(itemInfo.Text, Does.Contain("Components"));
 
-			var itemCount = driver.FindElement(By.Id("ItemCount"));
-			new Actions(driver).MoveToElement(itemCount).Click().SendKeys(Keys.Backspace).SendKeys("2").SendKeys(Keys.Enter).Perform();
+				var itemCount = driver.FindElement(By.Id("ItemCount"));
+				new Actions(driver).MoveToElement(itemCount).Click().SendKeys(Keys.Backspace).SendKeys("2").SendKeys(Keys.Enter).Perform();
 
-			itemInfo = driver.FindElement(By.Id(ItemInfoId));
-			Assert.That(itemInfo.Text, Does.Contain(item1InfoFor2));
+				itemInfo = driver.FindElement(By.Id(ItemInfoId));
+				Assert.That(itemInfo.Text, Does.Contain(item1InfoFor2));
+			}
+			catch
+			{
+				MakeScreen($"TestCase{testCaseNumber}_Exception");
 
-			MakeScreen($"TestCase{testCaseNumber}Finish");
+				throw;
+			}
 		}
 	}
 }
