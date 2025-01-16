Gruuuubik/FamilyIslandHelper

tests: Replace test parameters with test data JSON approach

FamilyIslandHelper.Web.Tests/HomeControllerTest.cs
@@ -36,41 +36,43 @@ namespace FamilyIslandHelper.Web.Tests
 			6. Set ItemCount = 2.
 				6.1. Check that item info is updated.
 		")]
-		[TestCase("ApiVersionHome1", 13, "JewelryWorkshop", "PearlEarrings", "01:15:00, 180", "03:15:00", "SapphireBracelet", "01:15:00, 485", 25, "Sackcloth", "Cloth", "02:30:00, 970", 1)]
-		[TestCase("ApiVersionHome2", 17, "Forge", "Needle", "00:08:00, 216", "00:08:00", "Glass", "00:35:00, 1928", 8, "Grass", "Axe", "01:10:00, 3856", 2)]
-		public void HomePageTest(string apiVersionId, int buildingsCount, string buildingId, string item1Id, string item1Info, string totalTimeInfo1, string item2Id, string item2Info, int expectedComponentsTreeImagesCount, string componentsTreeItemComponentImageId, string componentsTreeItemImageId, string item1InfoFor2, int testCaseNumber)
+		[TestCase("HomePage_TestCase1")]
+		[TestCase("HomePage_TestCase2")]
+		public void HomePageTest(string testDataJsonFileName)
 		{
 			try
 			{
+				var testDataDictionary = GetTestData(testDataJsonFileName);
+
 				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(17));
 
-				driver.FindElement(By.Id(apiVersionId)).Click();
+				driver.FindElement(By.Id(testDataDictionary["apiVersionId"])).Click();
 
-				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(buildingsCount));
+				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(int.Parse(testDataDictionary["buildingsCount"])));
 
-				var building = driver.FindElement(By.Id(buildingId));
+				var building = driver.FindElement(By.Id(testDataDictionary["buildingId"]));
 				building.Click();
 
-				building = driver.FindElement(By.Id(buildingId));
+				building = driver.FindElement(By.Id(testDataDictionary["buildingId"]));
 				Assert.That(building.GetAttribute("class"), Is.EqualTo(ClassesForSelectedBuilding));
 
-				var item1 = driver.FindElement(By.Id(item1Id));
+				var item1 = driver.FindElement(By.Id(testDataDictionary["item1Id"]));
 				Assert.That(item1.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 				var itemInfo = driver.FindElement(By.Id(ItemInfoId));
-				Assert.That(itemInfo.Text, Does.Contain(item1Info));
+				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item1Info"]));
 
 				var totalTimeInfo = driver.FindElement(By.Id("totalTimeInfo"));
-				Assert.That(totalTimeInfo.Text, Is.EqualTo(totalTimeInfo1));
+				Assert.That(totalTimeInfo.Text, Is.EqualTo(testDataDictionary["totalTimeInfo1"]));
 
-				var item2 = driver.FindElement(By.Id(item2Id));
+				var item2 = driver.FindElement(By.Id(testDataDictionary["item2Id"]));
 				Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage"));
 				item2.Click();
-				item2 = driver.FindElement(By.Id(item2Id));
+				item2 = driver.FindElement(By.Id(testDataDictionary["item2Id"]));
 				Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 				itemInfo = driver.FindElement(By.Id(ItemInfoId));
-				Assert.That(itemInfo.Text, Does.Contain(item2Info));
+				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item2Info"]));
 
 				driver.FindElement(By.Id("ShowListOfComponents")).Click();
 				itemInfo = driver.FindElement(By.Id(ItemInfoId));
@@ -78,22 +80,22 @@ namespace FamilyIslandHelper.Web.Tests
 
 				var componentsTree = driver.FindElement(By.Id("componentsTree"));
 				var componentsTreeImages = componentsTree.FindElements(By.TagName("img"));
-				Assert.That(componentsTreeImages, Has.Count.EqualTo(expectedComponentsTreeImagesCount));
+				Assert.That(componentsTreeImages, Has.Count.EqualTo(int.Parse(testDataDictionary["expectedComponentsTreeImagesCount"])));
 
-				var grass = driver.FindElement(By.Id(componentsTreeItemComponentImageId));
+				var grass = driver.FindElement(By.Id(testDataDictionary["componentsTreeItemComponentImageId"]));
 				Assert.That(grass.Displayed, Is.True);
-				driver.FindElement(By.Id(componentsTreeItemImageId)).Click();
+				driver.FindElement(By.Id(testDataDictionary["componentsTreeItemImageId"])).Click();
 				Assert.That(grass.Displayed, Is.False);
 
 				var itemCount = driver.FindElement(By.Id("ItemCount"));
 				new Actions(driver).MoveToElement(itemCount).Click().SendKeys(Keys.Backspace).SendKeys("2").SendKeys(Keys.Enter).Perform();
 
 				itemInfo = driver.FindElement(By.Id(ItemInfoId));
-				Assert.That(itemInfo.Text, Does.Contain(item1InfoFor2));
+				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item1InfoFor2"]));
 			}
 			catch
 			{
-				MakeScreen($"TestCase{testCaseNumber}_Exception");
+				MakeScreen($"{testDataJsonFileName}_Exception");
 
 				throw;
 			}

Gruuuubik/FamilyIslandHelper.Web

FamilyIslandHelper.Web.Tests/HomeControllerTest.cs
@@ -39,42 +39,44 @@ namespace FamilyIslandHelper.Web.Tests
 			7. Set ItemCount = 2.
 				7.1. Check that item info is updated.
 		")]
-		[TestCase("ApiVersionHome1", 13, "JewelryWorkshop", "PearlEarrings", "01:15:00, 180", "03:15:00", "SapphireBracelet", "01:15:00, 485", 25, "Sackcloth", "Cloth", "02:30:00, 970", 1)]
-		[TestCase("ApiVersionHome2", 17, "Forge", "Needle", "00:08:00, 216", "00:08:00", "Glass", "00:35:00, 1928", 8, "Grass", "Axe", "01:10:00, 3856", 2)]
-		public void HomePageTest(string apiVersionId, int buildingsCount, string buildingId, string item1Id, string item1Info, string totalTimeInfo1, string item2Id, string item2Info, int expectedComponentsTreeImagesCount, string componentsTreeItemComponentImageId, string componentsTreeItemImageId, string item1InfoFor2, int testCaseNumber)
+		[TestCase("HomePage_TestCase1")]
+		[TestCase("HomePage_TestCase2")]
+		public void HomePageTest(string testDataJsonFileName)
 		{
 			try
 			{
+				var testDataDictionary = GetTestData(testDataJsonFileName);
+
 				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(17));
 
-				driver.FindElement(By.Id(apiVersionId)).Click();
+				driver.FindElement(By.Id(testDataDictionary["apiVersionId"])).Click();
 
-				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(buildingsCount));
+				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(int.Parse(testDataDictionary["buildingsCount"])));
 
-				var building = driver.FindElement(By.Id(buildingId));
+				var building = driver.FindElement(By.Id(testDataDictionary["buildingId"]));
 				building.Click();
 
-				building = driver.FindElement(By.Id(buildingId));
+				building = driver.FindElement(By.Id(testDataDictionary["buildingId"]));
 				Assert.That(building.GetAttribute("class"), Is.EqualTo(ClassesForSelectedBuilding));
 
-				var item1 = driver.FindElement(By.Id(item1Id));
+				var item1 = driver.FindElement(By.Id(testDataDictionary["item1Id"]));
 				Assert.That(item1.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 				var itemInfo = driver.FindElement(By.Id(ItemInfoId));
-				Assert.That(itemInfo.Text, Does.Contain(item1Info));
+				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item1Info"]));
 
 				//3.4
 				var totalTimeInfo = driver.FindElement(By.Id("totalTimeInfo"));
-				Assert.That(totalTimeInfo.Text, Is.EqualTo(totalTimeInfo1));
+				Assert.That(totalTimeInfo.Text, Is.EqualTo(testDataDictionary["totalTimeInfo1"]));
 
-				var item2 = driver.FindElement(By.Id(item2Id));
+				var item2 = driver.FindElement(By.Id(testDataDictionary["item2Id"]));
 				Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage"));
 				item2.Click();
-				item2 = driver.FindElement(By.Id(item2Id));
+				item2 = driver.FindElement(By.Id(testDataDictionary["item2Id"]));
 				Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 				itemInfo = driver.FindElement(By.Id(ItemInfoId));
-				Assert.That(itemInfo.Text, Does.Contain(item2Info));
+				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item2Info"]));
 
 				driver.FindElement(By.Id("ShowListOfComponents")).Click();
 
@@ -85,12 +87,12 @@ namespace FamilyIslandHelper.Web.Tests
 				//5.2
 				var componentsTree = driver.FindElement(By.Id("componentsTree"));
 				var componentsTreeImages = componentsTree.FindElements(By.TagName("img"));
-				Assert.That(componentsTreeImages, Has.Count.EqualTo(expectedComponentsTreeImagesCount));
+				Assert.That(componentsTreeImages, Has.Count.EqualTo(int.Parse(testDataDictionary["expectedComponentsTreeImagesCount"])));
 
-				var itemComponentImage = driver.FindElement(By.Id(componentsTreeItemComponentImageId));
+				var itemComponentImage = driver.FindElement(By.Id(testDataDictionary["componentsTreeItemComponentImageId"]));
 				Assert.That(itemComponentImage.Displayed, Is.True);
-				driver.FindElement(By.Id(componentsTreeItemImageId)).Click();
-
+				driver.FindElement(By.Id(testDataDictionary["componentsTreeItemImageId"])).Click();
+				
 				//6.1
 				Assert.That(itemComponentImage.Displayed, Is.False);
 
@@ -99,11 +101,11 @@ namespace FamilyIslandHelper.Web.Tests
 				new Actions(driver).MoveToElement(itemCount).Click().SendKeys(Keys.Backspace).SendKeys("2").SendKeys(Keys.Enter).Perform();
 
 				itemInfo = driver.FindElement(By.Id(ItemInfoId));
-				Assert.That(itemInfo.Text, Does.Contain(item1InfoFor2));
+				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item1InfoFor2"]));
 			}
 			catch
 			{
-				MakeScreen($"TestCase{testCaseNumber}_Exception");
+				MakeScreen($"{testDataJsonFileName}_Exception");
 
 				throw;
 			}