5267c3fe8aea6ccbdd2137e490b51b593ccb9a42
Pavel Zhuraulevich <p.zhuravlevich@gmail.com>
2025-02-07 23:49:39 +0100

    tests: fix unit-tests after removing Home page

--- FamilyIslandHelper.Web.Tests/CompareControllerTest.cs
+++ FamilyIslandHelper.Web.Tests/CompareControllerTest.cs
@@ -20,6 +20,7 @@ namespace FamilyIslandHelper.Web.Tests
 		[Description(@"
 			1. Navigate to page Compare of FamilyIslandHelper.Web.
 			2. Select API version.
+				2.1. Check buildings count.
 			3. Check Component 1.
 				3.1. Select building.
 					3.1.1. Check that building is selected.
@@ -30,21 +31,22 @@ namespace FamilyIslandHelper.Web.Tests
 					3.2.1. Check that Item 2 from this building is selected.
 					3.2.2. Check that components tree contains all necessary components.
 			4. Turn on showing of components with time and energy.
-			5. Check Component 2.
-				5.1. Select building.
-					5.1.1. Check that building is selected.
-					5.1.2. Check that the 1st item from this building is selected.
-					5.1.3. Check item info.
-					5.1.4. Check total time info.
-					5.1.5. Check that another item from this building is not selected.
-				5.2. Select Item 2 from current building.
-					5.2.1. Check that Item 2 from this building is selected.
-					5.2.2. Check item info.
-					5.2.3. Check that item info contains Components info.
-					5.2.4. Check that components tree contains all necessary components.
-				5.4. Set ItemCount = 2.
-					5.4.1. Check that item info is updated.
-			6. Check item compare info.
+			5. Set Items count = 2.
+			6. Check Component 2.
+				6.1. Select building.
+					6.1.1. Check that building is selected.
+					6.1.2. Check that the 1st item from this building is selected.
+					6.1.3. Check item info.
+					6.1.4. Check total time info.
+					6.1.5. Check that another item from this building is not selected.
+				6.2. Select Item 2 from current building.
+					6.2.1. Check that Item 2 from this building is selected.
+					6.2.2. Check item info.
+					6.2.3. Check that item info contains Components info.
+					6.2.4. Check that components tree contains all necessary components.
+				6.3. Set ItemCount = 2.
+					6.3.1. Check that item info is updated.
+			7. Check item compare info.
 		")]
 		[TestCase("ComparePage_TestCase1")]
 		[TestCase("ComparePage_TestCase2")]
@@ -56,6 +58,8 @@ namespace FamilyIslandHelper.Web.Tests
 
 				driver.FindElement(By.Id(testDataDictionary["apiVersionId"])).Click();
 
+				Assert.That(driver.FindElements(By.ClassName("buildingImageName")), Has.Count.EqualTo(int.Parse(testDataDictionary["buildingsCount"])));
+
 				//3
 				CheckComponent(testDataDictionary, 1);
 
@@ -63,6 +67,9 @@ namespace FamilyIslandHelper.Web.Tests
 				driver.FindElement(By.Id("ShowComponentsWithTimeAndEnergyForAll")).Click();
 
 				//5
+				driver.FindElement(By.Id("ItemsCountOnView2")).Click();
+
+				//6
 				CheckComponent(testDataDictionary, 2);
 
 				Assert.That(driver.FindElement(By.Id("ItemCompareInfo")).Text, Is.Not.Empty);
@@ -91,57 +98,57 @@ namespace FamilyIslandHelper.Web.Tests
 			building = component.FindElement(By.Id(testDataDictionary["buildingId"]));
 			Assert.That(building.GetAttribute("class"), Is.EqualTo(ClassesForSelectedBuilding));
 
-			//3.1.2/5.1.2
+			//3.1.2/6.1.2
 			var item1 = component.FindElement(By.Id(testDataDictionary["item1Id"]));
 			Assert.That(item1.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 			if (componentNumber == 2)
 			{
-				//5.1.3
+				//6.1.3
 				var itemInfo = component.FindElement(By.Id(itemInfoId));
 				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item1Info"]));
 			}
 
-			//3.1.3/5.1.4
+			//3.1.3/6.1.4
 			var totalTimeInfo = driver.FindElement(By.Id(totalTimeInfoId));
 			Assert.That(totalTimeInfo.Text, Is.EqualTo(testDataDictionary["totalTimeInfo1"]));
 
-			//3.1.4/5.1.5
+			//3.1.4/6.1.5
 			var item2 = component.FindElement(By.Id(testDataDictionary["item2Id"]));
 			Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage"));
 
-			//3.2/5.2
+			//3.2/6.2
 			item2.Click();
 
-			//3.2.1/5.2.1
+			//3.2.1/6.2.1
 			component = driver.FindElement(By.Id(componentId));
 			item2 = component.FindElement(By.Id(testDataDictionary["item2Id"]));
 			Assert.That(item2.GetAttribute("class"), Is.EqualTo("itemNameImage selectedImage"));
 
 			if (componentNumber == 2)
 			{
-				//5.2.2
+				//6.2.2
 				var itemInfo = component.FindElement(By.Id(itemInfoId));
 				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item2Info"]));
 
-				//5.2.3
+				//6.2.3
 				component = driver.FindElement(By.Id(componentId));
 				itemInfo = component.FindElement(By.Id(itemInfoId));
 				Assert.That(itemInfo.Text, Does.Contain("Components"));
 			}
 
-			//3.2.2/5.2.4
+			//3.2.2/6.2.4
 			var componentsTree = driver.FindElement(By.Id(componentsTreeId));
 			var componentsTreeImages = componentsTree.FindElements(By.TagName("img"));
 			Assert.That(componentsTreeImages, Has.Count.EqualTo(int.Parse(testDataDictionary["expectedComponentsTreeImagesCount"])));
 
 			if (componentNumber == 2)
 			{
-				//5.4
+				//6.3
 				var itemCount = component.FindElement(By.Id(itemCountId));
 				new Actions(driver).MoveToElement(itemCount).Click().SendKeys(Keys.Backspace).SendKeys("2").SendKeys(Keys.Enter).Perform();
 
-				//5.4.1
+				//6.3.1
 				component = driver.FindElement(By.Id(componentId));
 				var itemInfo = component.FindElement(By.Id(itemInfoId));
 				Assert.That(itemInfo.Text, Does.Contain(testDataDictionary["item1InfoFor2"]));
